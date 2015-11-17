d4b2a4d5022e1e89febea827f64f6ce799ac16ed
urb4nm <urb4nm@gmail.com>
2015-11-17 20:36:00 +0100

    Initial commit, with starting layout

---
+++ RPGCharacterCreator/Models/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPGCharacterCreator.Models
+{
+    [Authorize]
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ClassChoice { get; set; }
+        public SelectList ClassChoices { get; set; }
+        public int StrengthPoints { get; set; }
+        public int IntelligencePoints { get; set; }
+        public int AgilityPoints { get; set; }
+        public int MaxPoints { get; set; }
+    }
+}
