890c5065463ec4c09cfa87751b032740fb243710
urb4nm <urb4nm@gmail.com>
2015-11-18 13:58:07 +0100

    Added CharacterController

---
+++ RPGCharacterCreator/Controllers/CharacterController.cs
@@ -0,0 +1,98 @@
+﻿using RPGCharacterCreator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPGCharacterCreator.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Character/Details
+        public ActionResult Details()
+        {
+            var character = new Character
+            {
+                Name = "test",
+                StrengthPoints = 5,
+                IntelligencePoints = 5,
+                AgilityPoints = 5,
+                ClassChoice = "Mage"
+            };
+            return View(character);
+        }
+
+        // GET: Character/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Character/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
