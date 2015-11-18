0fb23ad619b4d7d4e6d8a9cc7f922fc9c4b7396c
urb4nm <urb4nm@gmail.com>
2015-11-18 13:57:47 +0100

    Added Character model and Character Detail View

--- RPGCharacterCreator/Models/Character.cs
+++ RPGCharacterCreator/Models/Character.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,40 @@ namespace RPGCharacterCreator.Models
     public class Character
     {
         public int Id { get; set; }
+        
+        [Required]
+        [Display(Name="Name")]
+        [StringLength(60)]
         public string Name { get; set; }
+
         public string ClassChoice { get; set; }
+        
+        //[Required]
         public SelectList ClassChoices { get; set; }
+        
+        [Required]
+        [Display(Name="Strength")]
+        [Range(0, 10, ErrorMessage="Incorrect value")]
         public int StrengthPoints { get; set; }
+        
+        [Required]
+        [Display(Name = "Intelligence")]
+        [Range(0, 10, ErrorMessage = "Incorrect value")]
         public int IntelligencePoints { get; set; }
+        
+        [Required]
+        [Display(Name = "Agility")]
+        [Range(0, 10, ErrorMessage = "Incorrect value")]
         public int AgilityPoints { get; set; }
+
         public int MaxPoints { get; set; }
+
+        public Character()
+        {
+            MaxPoints = 15;
+            ClassChoices = new SelectList(new string[] { "Mage", "Knight", "Rouge" });
+        }
+
+
     }
 }
